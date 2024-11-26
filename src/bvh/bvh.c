86fb55eee93b1fa353c6c8499048fdbb02e7aba5
Lingyi Kong <irwen817@gmail.com>
2024-11-26 15:55:59 +0200

    test

--- src/bvh/bvh.c
+++ src/bvh/bvh.c
@@ -6,13 +6,21 @@
 /*   By: likong <likong@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 21:22:53 by likong            #+#    #+#             */
-/*   Updated: 2024/11/25 21:29:41 by likong           ###   ########.fr       */
+/*   Updated: 2024/11/26 15:45:45 by likong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-t_bvh	init_bvh()
+t_bvh	*init_bvh()
 {
-	
+	t_bvh	*res;
+
+	if (!s()->shapes)
+		return (NULL);
+	res = ft_calloc(1, sizeof(t_bvh));
+	if (!res)
+		return (NULL);
+	res->id = 
+	return (res);
 }
