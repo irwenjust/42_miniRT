b7efbc915f43eee97f94be0e9d8d5a8af57d8b40
Lingyi Kong <irwen817@gmail.com>
2024-11-27 21:52:55 +0200

    hide bvh condition for normal debug

--- src/renderer/render.c
+++ src/renderer/render.c
@@ -6,7 +6,7 @@
 /*   By: likong <likong@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 16:35:15 by yzhan             #+#    #+#             */
-/*   Updated: 2024/11/27 21:42:16 by likong           ###   ########.fr       */
+/*   Updated: 2024/11/27 21:49:14 by likong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,8 +101,9 @@ void	fake_render()
 			closest.distance = INFINITY;
 			converted_cur = convert_viewport(cur.x, cur.y);
 			ray = make_ray(converted_cur);
-			if (check_bvh_intersection(&ray, s()->bvh, &closest)
-				&& check_intersection(s()->shapes, &ray, &closest))
+			// if (check_bvh_intersection(&ray, s()->bvh, &closest)
+			// 	&& check_intersection(s()->shapes, &ray, &closest))
+			if (check_intersection(s()->shapes, &ray, &closest))
 				check_illumination(&closest); //4-if intersect, update closest color based on other env
 			put_pixel(closest.color, cur.x, cur.y);
 			cur.x += 3;
