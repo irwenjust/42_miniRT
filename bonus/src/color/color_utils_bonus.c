0257b17a09aa4dc2c1bb69b4280873f9c1b20ebe
Ying Zhan <yzhan@c1r1p4.hive.fi>
2025-02-17 16:43:57 +0200

    final?

--- bonus/src/color/color_utils_bonus.c
+++ bonus/src/color/color_utils_bonus.c
@@ -27,7 +27,7 @@ void	put_pixel(t_color c, int x, int y)
 {
 	char	*dst;
 
-	dst = s()->win.addr + (y * WIDTH + x) * (int)(s()->win.bpp * 0.125);
+	dst = s()->win.addr + (y * (int)WIDTH + x) * (int)(s()->win.bpp * 0.125);
 	*(unsigned int *)dst = (c.alpha << 24 | c.red << 16
 			| c.green << 8 | c.blue);
 }
